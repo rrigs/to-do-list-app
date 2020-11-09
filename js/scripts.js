0aaec9677f58304e72abcc2b8cf62d5cccf242c8
rrigs <71404266+rrigs@users.noreply.github.com>
2020-11-09 13:10:50 -0500

    converted section 1 code to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 function newItem() {
-  //javascript
   //1. Adding a new item to the list of items:
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let text = document.createTextNode(inputValue);
-  li.appendChild(text);
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
-  if (inputValue === "") {
+  if (inputValue === '') {
     alert("You must write something!");
   } else {
-    let list = document.querySelector("#list");
-    list.appendChild(li);
+      let list = $('#list');
+      $('#list').append(li);
   }
 
   //2. Crossing out an item from the list of items:
