98bb7df3c481359c531b83709ce4318fa4368194
rrigs <71404266+rrigs@users.noreply.github.com>
2020-11-09 13:17:42 -0500

    converted section 3i code to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,12 @@ function newItem() {
   li.on("dblclick", crossOut);
 
   //3(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
 
-  crossOutButton.addEventListener("click", deleteListItem);
+    crossOutButton.on("click", deleteListItem);
+    
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem() {
     li.classList.add("delete");
